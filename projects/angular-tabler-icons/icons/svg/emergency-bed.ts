6cc5b5f01d6a97c53927ed7a8731e7daf3ba57d7
pierreavn <pierreavn@users.noreply.github.com>
2021-11-17 13:53:07 +0000

    Tabler Icons v1.44.0

---
+++ projects/angular-tabler-icons/icons/svg/emergency-bed.ts
@@ -0,0 +1,13 @@
+export const IconEmergencyBed = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="16" cy="18" r="2" />
+  <circle cx="8" cy="18" r="2" />
+  <path d="M4 8l2.1 2.8a3 3 0 0 0 2.4 1.2h11.5" />
+  <path d="M10 6h4" />
+  <path d="M12 4v4" />
+  <path d="M12 12v2l-2.5 2.5" />
+  <path d="M14.5 16.5l-2.5 -2.5" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/pills.ts
@@ -0,0 +1,10 @@
+export const IconPills = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="8" cy="8" r="5" />
+  <circle cx="17" cy="17" r="4" />
+  <path d="M4.5 4.5l7 7" />
+  <path d="M19.5 14.5l-5 5" />
+</svg>
+
+
+`
