b7df7bc23bf556ebb9b8858b73408d21f677ce38
pierreavn <pierreavn@users.noreply.github.com>
2021-11-29 06:55:49 +0000

    Tabler Icons v1.46.0

---
+++ projects/angular-tabler-icons/icons/svg/user-search.ts
@@ -0,0 +1,10 @@
+export const IconUserSearch = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="7" r="4" />
+  <path d="M6 21v-2a4 4 0 0 1 4 -4h1" />
+  <circle cx="16.5" cy="17.5" r="2.5" />
+  <path d="M18.5 19.5l2.5 2.5" />
+</svg>
+
+
+`
