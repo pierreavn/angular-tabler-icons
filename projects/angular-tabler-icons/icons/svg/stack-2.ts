8e31fb5525108fef2830b453b0b7af5a6966f083
pierreavn <pierreavn@users.noreply.github.com>
2021-12-09 06:41:02 +0000

    Tabler Icons v1.47.0

--- projects/angular-tabler-icons/icons/svg/stack-2.ts
+++ projects/angular-tabler-icons/icons/svg/stack-2.ts
@@ -1,7 +1,8 @@
 export const IconStack2 = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M12 6l-8 4l8 4l8 -4l-8 -4" />
-  <path d="M4 14l8 4l8 -4" />
+  <polyline points="12 4 4 8 12 12 20 8 12 4" />
+  <polyline points="4 12 12 16 20 12" />
+  <polyline points="4 16 12 20 20 16" />
 </svg>
 
 
