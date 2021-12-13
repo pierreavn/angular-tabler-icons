5111765793153868f16583f6cc161e2886fc45a8
pierreavn <pierreavn@users.noreply.github.com>
2021-12-13 15:10:19 +0000

    Tabler Icons v1.48.0

---
+++ projects/angular-tabler-icons/icons/svg/currency.ts
@@ -0,0 +1,11 @@
+export const IconCurrency = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="12" r="7" />
+  <path d="M4 4l3 3" />
+  <path d="M20 4l-3 3" />
+  <path d="M4 20l3 -3" />
+  <path d="M20 20l-3 -3" />
+</svg>
+
+
+`
