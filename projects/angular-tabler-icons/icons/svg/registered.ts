f7d9a161dc2df819a27aa54e3dad83bbfde24152
pierreavn <pierreavn@users.noreply.github.com>
2021-12-25 22:54:27 +0000

    Tabler Icons v1.49.0

--- projects/angular-tabler-icons/icons/svg/registered.ts
+++ projects/angular-tabler-icons/icons/svg/registered.ts
@@ -1,7 +1,8 @@
 export const IconRegistered = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <circle cx="12" cy="12" r="9" />
-  <path d="M9 16v-8h4a2 2 0 0 1 0 4h-4m3 0l3 4" />
+  <path d="M10 15v-6h2a2 2 0 1 1 0 4h-2" />
+  <path d="M14 15l-2 -2" />
 </svg>
 
 
