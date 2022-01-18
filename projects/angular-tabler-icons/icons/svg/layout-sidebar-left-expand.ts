5092073bdcf69d61126630ab11148a23454fcbd2
pierreavn <pierreavn@users.noreply.github.com>
2022-01-18 20:57:52 +0000

    Tabler Icons v1.53.0

---
+++ projects/angular-tabler-icons/icons/svg/layout-sidebar-left-expand.ts
@@ -0,0 +1,9 @@
+export const IconLayoutSidebarLeftExpand = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <rect x="4" y="4" width="16" height="16" rx="2" />
+  <path d="M9 4v16" />
+  <path d="M14 10l2 2l-2 2" />
+</svg>
+
+
+`
