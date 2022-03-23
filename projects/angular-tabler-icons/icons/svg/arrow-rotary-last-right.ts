1b425a3a6c88aea3d15fac8a935cdc06411f79b3
pierreavn <pierreavn@users.noreply.github.com>
2022-03-23 05:58:40 +0000

    Tabler Icons v1.57.0

---
+++ projects/angular-tabler-icons/icons/svg/arrow-rotary-last-right.ts
@@ -0,0 +1,10 @@
+export const IconArrowRotaryLastRight = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="9" cy="12" r="3" />
+  <path d="M9 15v6" />
+  <path d="M11.5 9.5l6.5 -6.5" />
+  <path d="M13 3h5v5" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/arrow-rotary-right.ts
@@ -0,0 +1,10 @@
+export const IconArrowRotaryRight = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="8" cy="7" r="3" />
+  <path d="M8 10v10" />
+  <path d="M17 11l4 -4l-4 -4" />
+  <path d="M11 7h10" />
+</svg>
+
+
+`
