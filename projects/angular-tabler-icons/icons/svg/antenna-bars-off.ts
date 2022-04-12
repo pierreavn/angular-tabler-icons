e1edc5b412a18f632204d8a877c73b58e525ae45
pierreavn <pierreavn@users.noreply.github.com>
2022-04-12 07:01:03 +0000

    Tabler Icons v1.64.0

--- projects/angular-tabler-icons/icons/svg/antenna-bars-off.ts
+++ projects/angular-tabler-icons/icons/svg/antenna-bars-off.ts
@@ -3,6 +3,7 @@ export const IconAntennaBarsOff = `<svg xmlns="http://www.w3.org/2000/svg" viewB
   <path d="M6 18v-3" />
   <path d="M10 18v-6" />
   <path d="M14 18v-4" />
+  <path d="M14 10v-1" />
   <path d="M18 14v-8" />
   <path d="M3 3l18 18" />
 </svg>
---
+++ projects/angular-tabler-icons/icons/svg/gender-agender.ts
@@ -0,0 +1,8 @@
+export const IconGenderAgender = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="12" r="6" />
+  <path d="M7 12h11" />
+</svg>
+
+
+`
