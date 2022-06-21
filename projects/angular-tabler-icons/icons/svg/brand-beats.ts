a282fba7e2bc6cdd38c6776fac5127b43e3034d6
pierreavn <pierreavn@users.noreply.github.com>
2022-06-21 10:19:16 +0000

    Tabler Icons v1.71.0

---
+++ projects/angular-tabler-icons/icons/svg/brand-beats.ts
@@ -0,0 +1,9 @@
+export const IconBrandBeats = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="12" r="9" />
+  <circle cx="12.5" cy="12.5" r="3.5" />
+  <path d="M9 12v-8" />
+</svg>
+
+
+`
