892257c8547e0225b20c4450b07c16b2c12d554b
pierreavn <pierreavn@users.noreply.github.com>
2022-06-27 06:09:33 +0000

    Tabler Icons v1.72.0

---
+++ projects/angular-tabler-icons/icons/svg/brand-xing.ts
@@ -0,0 +1,8 @@
+export const IconBrandXing = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M16 21l-4 -7l6.5 -11" />
+  <path d="M7 7l2 3.5l-3 4.5" />
+</svg>
+
+
+`
