475e02aea527a430d952279bbcb0cbd197fd75c4
pierreavn <pierreavn@users.noreply.github.com>
2022-07-09 19:12:13 +0000

    Tabler Icons v1.73.0

---
+++ projects/angular-tabler-icons/icons/svg/arrow-guide.ts
@@ -0,0 +1,9 @@
+export const IconArrowGuide = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="5" cy="19" r="2" />
+  <path d="M7 19h3a2 2 0 0 0 2 -2v-8a2 2 0 0 1 2 -2h7" />
+  <path d="M18 4l3 3l-3 3" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/brand-appgallery.ts
@@ -0,0 +1,8 @@
+export const IconBrandAppgallery = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <rect x="4" y="4" width="16" height="16" rx="4" />
+  <path d="M9 8a3 3 0 0 0 6 0" />
+</svg>
+
+
+`
