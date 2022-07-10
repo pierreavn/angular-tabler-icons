ddb5eb9fb9e2a4c0f5dd81f08f3183a9ab0a3687
pierreavn <pierreavn@users.noreply.github.com>
2022-07-10 10:49:31 +0000

    Tabler Icons v1.74.0

---
+++ projects/angular-tabler-icons/icons/svg/storm.ts
@@ -0,0 +1,10 @@
+export const IconStorm = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="12" r="3" />
+  <circle cx="12" cy="12" r="7" />
+  <path d="M5.369 14.236c-1.839 -3.929 -1.561 -7.616 -.704 -11.236" />
+  <path d="M18.63 9.76c1.837 3.928 1.561 7.615 .703 11.236" />
+</svg>
+
+
+`
