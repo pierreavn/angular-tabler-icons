8b5e65de07408e10e8b5e5ed45c1a6ae36c92d21
pierreavn <pierreavn@users.noreply.github.com>
2022-07-16 07:09:46 +0000

    Tabler Icons v1.76.0

---
+++ projects/angular-tabler-icons/icons/svg/rating-18-plus.ts
@@ -0,0 +1,12 @@
+export const IconRating18Plus = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="12" r="9" />
+  <circle cx="11.5" cy="10.5" r="1.5" />
+  <circle cx="11.5" cy="13.5" r="1.5" />
+  <path d="M7 15v-6" />
+  <path d="M15.5 12h3" />
+  <path d="M17 10.5v3" />
+</svg>
+
+
+`
