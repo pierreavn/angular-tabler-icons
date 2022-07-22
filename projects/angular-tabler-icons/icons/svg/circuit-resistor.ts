6ee87a871c4fcaa082610e6d7172b1d05b0747b5
pierreavn <pierreavn@users.noreply.github.com>
2022-07-22 09:23:29 +0000

    Tabler Icons v1.77.0

---
+++ projects/angular-tabler-icons/icons/svg/circuit-resistor.ts
@@ -0,0 +1,7 @@
+export const IconCircuitResistor = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M2 12h2l2 -5l3 10l3 -10l3 10l3 -10l1.5 5h2.5" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/circuit-switch-closed.ts
@@ -0,0 +1,11 @@
+export const IconCircuitSwitchClosed = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M2 12h2" />
+  <path d="M20 12h2" />
+  <circle cx="6" cy="12" r="2" />
+  <circle cx="18" cy="12" r="2" />
+  <path d="M8 12h8" />
+</svg>
+
+
+`
