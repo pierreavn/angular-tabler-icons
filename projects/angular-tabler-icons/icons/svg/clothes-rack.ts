a3b6b1c1016a3feb67c0bbf3fa0c0ea4bbf8d3fe
pierreavn <pierreavn@users.noreply.github.com>
2022-07-23 11:38:19 +0000

    Tabler Icons v1.78.0

---
+++ projects/angular-tabler-icons/icons/svg/clothes-rack.ts
@@ -0,0 +1,10 @@
+export const IconClothesRack = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="5" r="2" />
+  <path d="M12 7v14" />
+  <path d="M9 21h6" />
+  <path d="M7.757 9.243a6 6 0 0 0 8.486 0" />
+</svg>
+
+
+`
