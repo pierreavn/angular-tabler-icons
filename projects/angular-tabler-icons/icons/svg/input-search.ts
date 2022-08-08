d59405492e991ef9af1d71ed6c1c84923c768be4
pierreavn <pierreavn@users.noreply.github.com>
2022-08-08 14:34:42 +0000

    Tabler Icons v1.79.0

---
+++ projects/angular-tabler-icons/icons/svg/input-search.ts
@@ -0,0 +1,9 @@
+export const IconInputSearch = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M19 11v-3a2 2 0 0 0 -2 -2h-12a2 2 0 0 0 -2 2v5a2 2 0 0 0 2 2h5" />
+  <circle cx="15.5" cy="15.5" r="2.5" />
+  <path d="M17.5 17.5l2.5 2.5" />
+</svg>
+
+
+`
