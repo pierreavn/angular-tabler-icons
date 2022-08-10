108c946a5e95b3d3d66a818ba3a529090edd6bcc
pierreavn <pierreavn@users.noreply.github.com>
2022-08-10 12:12:57 +0000

    Tabler Icons v1.80.0

---
+++ projects/angular-tabler-icons/icons/svg/playstation-square.ts
@@ -0,0 +1,8 @@
+export const IconPlaystationSquare = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12 21a9 9 0 0 0 9 -9a9 9 0 0 0 -9 -9a9 9 0 0 0 -9 9a9 9 0 0 0 9 9z" />
+  <rect x="8" y="8" width="8" height="8" rx="1" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/transition-bottom.ts
@@ -0,0 +1,10 @@
+export const IconTransitionBottom = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M21 18a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3" />
+  <rect x="3" y="3" width="18" height="6" rx="3" />
+  <path d="M12 9v8" />
+  <path d="M9 14l3 3l3 -3" />
+</svg>
+
+
+`
