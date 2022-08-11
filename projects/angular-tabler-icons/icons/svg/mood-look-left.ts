20c331f8c3349832b282c98040c27d9a56e76358
pierreavn <pierreavn@users.noreply.github.com>
2022-08-11 10:33:13 +0000

    Tabler Icons v1.81.0

---
+++ projects/angular-tabler-icons/icons/svg/mood-look-left.ts
@@ -0,0 +1,9 @@
+export const IconMoodLookLeft = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="12" r="9" />
+  <path d="M9 9h.01" />
+  <path d="M4 15h4" />
+</svg>
+
+
+`
