ff6243317fc9e802aef3d703bb515206a1f540ad
pierreavn <pierreavn@users.noreply.github.com>
2022-08-17 06:56:01 +0000

    Tabler Icons v1.83.1

---
+++ projects/angular-tabler-icons/icons/svg/mood-unamused.ts
@@ -0,0 +1,10 @@
+export const IconMoodUnamused = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="12" r="9" />
+  <path d="M11 16l4 -1.5" />
+  <path d="M10 10c-.5 -1 -2.5 -1 -3 0" />
+  <path d="M17 10c-.5 -1 -2.5 -1 -3 0" />
+</svg>
+
+
+`
