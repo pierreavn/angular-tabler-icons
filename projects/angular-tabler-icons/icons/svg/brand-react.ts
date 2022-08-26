70e75157fa1b1f74e613706e5cfd3f7cbce52fc6
pierreavn <pierreavn@users.noreply.github.com>
2022-08-26 06:52:32 +0000

    Tabler Icons v1.88.0

---
+++ projects/angular-tabler-icons/icons/svg/brand-react.ts
@@ -0,0 +1,10 @@
+export const IconBrandReact = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <ellipse cx="12" cy="12" rx="4" ry="10" />
+  <ellipse transform="rotate(60 12 12)" cx="12" cy="12" rx="4" ry="10" />
+  <ellipse transform="rotate(-60 12 12)" cx="12" cy="12" rx="4" ry="10" />
+  <circle fill="#D8D8D8" cx="12" cy="12" r="1" />
+</svg>
+
+
+`
