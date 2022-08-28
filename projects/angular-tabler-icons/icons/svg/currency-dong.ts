c5aee6738987aa76757c3c62d3b633833d7c8f16
pierreavn <pierreavn@users.noreply.github.com>
2022-08-28 06:59:36 +0000

    Tabler Icons v1.90.0

---
+++ projects/angular-tabler-icons/icons/svg/currency-dong.ts
@@ -0,0 +1,10 @@
+export const IconCurrencyDong = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M6 19h12" />
+  <circle cx="12" cy="12" r="4" />
+  <path d="M16 16v-12" />
+  <path d="M17 5h-4" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/currency-quetzal.ts
@@ -0,0 +1,8 @@
+export const IconCurrencyQuetzal = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="12" r="6" />
+  <path d="M13 13l5 5" />
+</svg>
+
+
+`
