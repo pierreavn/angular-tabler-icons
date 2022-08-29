10e860e0a0f7d0c84ebb968e6cc147d5a14d3929
pierreavn <pierreavn@users.noreply.github.com>
2022-08-29 10:22:42 +0000

    Tabler Icons v1.91.0

---
+++ projects/angular-tabler-icons/icons/svg/chart-treemap.ts
@@ -0,0 +1,12 @@
+export const IconChartTreemap = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <rect x="4" y="4" width="16" height="16" rx="2" />
+  <path d="M12 4v16" />
+  <path d="M4 15h8" />
+  <path d="M12 12h8" />
+  <path d="M16 12v8" />
+  <path d="M16 16h4" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/layout-collage.ts
@@ -0,0 +1,9 @@
+export const IconLayoutCollage = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <rect x="4" y="4" width="16" height="16" rx="2" />
+  <path d="M10 4l4 16" />
+  <path d="M12 12l-8 2" />
+</svg>
+
+
+`
