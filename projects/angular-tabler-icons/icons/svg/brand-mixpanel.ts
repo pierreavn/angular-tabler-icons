ccabb307fd18e32573f8f131d966d041ab38e9b0
pierreavn <pierreavn@users.noreply.github.com>
2022-09-02 14:20:47 +0000

    Tabler Icons v1.92.0

---
+++ projects/angular-tabler-icons/icons/svg/brand-mixpanel.ts
@@ -0,0 +1,9 @@
+export const IconBrandMixpanel = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="4.5" cy="12" r="2.5" />
+  <circle cx="20.5" cy="12" r="1.5" />
+  <circle cx="13" cy="12" r="2" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/brand-upwork.ts
@@ -0,0 +1,7 @@
+export const IconBrandUpwork = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M3 7v5a3 3 0 0 0 6 0v-5h1l4 6c.824 1.319 1.945 2 3.5 2a3.5 3.5 0 0 0 0 -7c-2.027 0 -3.137 1.006 -3.5 3c-.242 1.33 -.908 3.996 -2 8" />
+</svg>
+
+
+`
