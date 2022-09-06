0403009de390e38a3e3bd43be81eea513704b883
pierreavn <pierreavn@users.noreply.github.com>
2022-09-06 12:11:58 +0000

    Tabler Icons v1.93.0

---
+++ projects/angular-tabler-icons/icons/svg/status-change.ts
@@ -0,0 +1,10 @@
+export const IconStatusChange = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="6" cy="18" r="2" />
+  <circle cx="18" cy="18" r="2" />
+  <path d="M6 12v-2a6 6 0 1 1 12 0v2" />
+  <path d="M15 9l3 3l3 -3" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/txt.ts
@@ -0,0 +1,12 @@
+export const IconTxt = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M3 8h4" />
+  <path d="M5 8v8" />
+  <path d="M17 8h4" />
+  <path d="M19 8v8" />
+  <path d="M10 8l4 8" />
+  <path d="M10 16l4 -8" />
+</svg>
+
+
+`
