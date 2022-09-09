ee516d2c7d2bdf73cb885f5602cdb0da452bfb58
pierreavn <pierreavn@users.noreply.github.com>
2022-09-09 06:54:23 +0000

    Tabler Icons v1.95.0

--- projects/angular-tabler-icons/icons/svg/circle-0.ts
+++ projects/angular-tabler-icons/icons/svg/circle-0.ts
@@ -1,7 +1,7 @@
 export const IconCircle0 = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M12 8a2 2 0 0 1 2 2v4a2 2 0 1 1 -4 0v-4a2 2 0 0 1 2 -2z" />
   <circle cx="12" cy="12" r="9" />
+  <path d="M10 10v4a2 2 0 1 0 4 0v-4a2 2 0 1 0 -4 0z" />
 </svg>
 
 
---
+++ projects/angular-tabler-icons/icons/svg/square-w.ts
@@ -0,0 +1,8 @@
+export const IconSquareW = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <rect x="4" y="4" width="16" height="16" rx="2" />
+  <path d="M9 8l1 8l2 -5l2 5l1 -8" />
+</svg>
+
+
+`
