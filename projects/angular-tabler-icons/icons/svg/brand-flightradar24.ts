8037ddf75734081c508f584be07c846d09295f5d
pierreavn <pierreavn@users.noreply.github.com>
2022-09-23 15:38:55 +0000

    Tabler Icons v1.99.0

---
+++ projects/angular-tabler-icons/icons/svg/brand-flightradar24.ts
@@ -0,0 +1,10 @@
+export const IconBrandFlightradar24 = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="12" r="9" />
+  <circle cx="12" cy="12" r="5" />
+  <path d="M8.5 20l3.5 -8l-6.5 6" />
+  <circle cx="12" cy="12" r="1" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/e-passport.ts
@@ -0,0 +1,10 @@
+export const IconEPassport = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <rect x="2" y="5" width="20" height="14" rx="1.999" />
+  <circle cx="12" cy="12" r="3" />
+  <path d="M9 12h-7" />
+  <path d="M15 12h7" />
+</svg>
+
+
+`
