22368aa0c034f2ec7ce2a762ade1d660978cf173
pierreavn <pierreavn@users.noreply.github.com>
2022-09-27 06:42:51 +0000

    Tabler Icons v1.101.0

---
+++ projects/angular-tabler-icons/icons/svg/perfume.ts
@@ -0,0 +1,11 @@
+export const IconPerfume = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M10 6v3" />
+  <path d="M14 6v3" />
+  <rect x="5" y="9" width="14" height="12" rx="2" />
+  <circle cx="12" cy="15" r="2" />
+  <path d="M9 3h6v3h-6z" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/spray.ts
@@ -0,0 +1,15 @@
+export const IconSpray = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <rect x="4" y="10" width="8" height="11" rx="2" />
+  <path d="M6 10v-4a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v4" />
+  <path d="M15 7h.01" />
+  <path d="M18 9h.01" />
+  <path d="M18 5h.01" />
+  <path d="M21 3h.01" />
+  <path d="M21 7h.01" />
+  <path d="M21 11h.01" />
+  <path d="M10 7h1" />
+</svg>
+
+
+`
