8433b3406c7e018f1bb21530203861a01e882db2
pierreavn <pierreavn@users.noreply.github.com>
2022-10-10 12:35:06 +0000

    Tabler Icons v1.102.0

---
+++ projects/angular-tabler-icons/icons/svg/s-turn-right.ts
@@ -0,0 +1,9 @@
+export const IconSTurnRight = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="5" cy="5" r="2" />
+  <path d="M7 5h9.5a3.5 3.5 0 0 1 0 7h-9a3.5 3.5 0 0 0 0 7h13.5" />
+  <path d="M18 16l3 3l-3 3" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/tic-tac.ts
@@ -0,0 +1,14 @@
+export const IconTicTac = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="6" cy="6" r="2" />
+  <path d="M3 12h18" />
+  <path d="M12 3v18" />
+  <path d="M4 16l4 4" />
+  <path d="M4 20l4 -4" />
+  <path d="M16 4l4 4" />
+  <path d="M16 8l4 -4" />
+  <circle cx="18" cy="18" r="2" />
+</svg>
+
+
+`
