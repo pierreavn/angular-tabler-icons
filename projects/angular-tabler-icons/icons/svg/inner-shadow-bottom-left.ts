c15def99825047bae72631f181c8197f18ff59a6
pierreavn <pierreavn@users.noreply.github.com>
2022-10-12 06:53:21 +0000

    Tabler Icons v1.103.0

---
+++ projects/angular-tabler-icons/icons/svg/inner-shadow-bottom-left.ts
@@ -0,0 +1,8 @@
+export const IconInnerShadowBottomLeft = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="12" r="9" />
+  <path d="M6 12a6 6 0 0 0 6 6" />
+</svg>
+
+
+`
