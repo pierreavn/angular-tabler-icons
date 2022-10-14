3593417a733e9dfb188387faa884edc7db4ff78b
pierreavn <pierreavn@users.noreply.github.com>
2022-10-14 06:37:47 +0000

    Tabler Icons v1.104.0

---
+++ projects/angular-tabler-icons/icons/svg/align-box-bottom-left.ts
@@ -0,0 +1,10 @@
+export const IconAlignBoxBottomLeft = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <rect x="4" y="4" width="16" height="16" rx="2" />
+  <path d="M7 15v2" />
+  <path d="M10 11v6" />
+  <path d="M13 13v4" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/lambda.ts
@@ -0,0 +1,8 @@
+export const IconLambda = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M6 20l6.5 -9" />
+  <path d="M19 20c-6 0 -6 -16 -12 -16" />
+</svg>
+
+
+`
