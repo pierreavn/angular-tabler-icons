f89478c4f78eb1c8c15d06ce809c299e0660f435
pierreavn <pierreavn@users.noreply.github.com>
2022-10-18 05:44:14 +0000

    Tabler Icons v1.105.0

---
+++ projects/angular-tabler-icons/icons/svg/cylinder.ts
@@ -0,0 +1,8 @@
+export const IconCylinder = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <ellipse cx="12" cy="6" rx="5" ry="3" />
+  <path d="M7 6v12c0 1.657 2.239 3 5 3s5 -1.343 5 -3v-12" />
+</svg>
+
+
+`
