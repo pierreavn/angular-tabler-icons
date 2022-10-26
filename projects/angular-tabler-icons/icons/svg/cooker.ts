4a13d738a47695e9287f391a355ef7dbe2a3b066
pierreavn <pierreavn@users.noreply.github.com>
2022-10-26 16:09:52 +0000

    Tabler Icons v1.108.0

---
+++ projects/angular-tabler-icons/icons/svg/cooker.ts
@@ -0,0 +1,12 @@
+export const IconCooker = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12 7h.01" />
+  <path d="M15 7h.01" />
+  <path d="M9 7h.01" />
+  <rect x="5" y="3" width="14" height="18" rx="2" />
+  <path d="M9 15h6" />
+  <path d="M5 11h14" />
+</svg>
+
+
+`
