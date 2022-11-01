887d450bf4d0865466d6047e7579bceabb65da6b
pierreavn <pierreavn@users.noreply.github.com>
2022-11-01 11:26:24 +0000

    Tabler Icons v1.109.0

---
+++ projects/angular-tabler-icons/icons/svg/confucius.ts
@@ -0,0 +1,10 @@
+export const IconConfucius = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M9 19l3 2v-18" />
+  <path d="M4 10l8 -2" />
+  <path d="M4 18l8 -10" />
+  <path d="M20 18l-8 -8l8 -4" />
+</svg>
+
+
+`
