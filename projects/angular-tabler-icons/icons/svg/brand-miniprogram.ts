b492a79af2fb0d869fe0391953059469e37a1877
pierreavn <pierreavn@users.noreply.github.com>
2022-11-28 19:46:07 +0000

    Tabler Icons v1.114.0

---
+++ projects/angular-tabler-icons/icons/svg/brand-miniprogram.ts
@@ -0,0 +1,8 @@
+export const IconBrandMiniprogram = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M21 12a9 9 0 1 1 -18 0a9 9 0 0 1 18 0z" />
+  <path d="M7.996 11.503a2.5 2.5 0 1 0 4.004 1.997v-3a2.5 2.5 0 1 1 3.995 2.004" />
+</svg>
+
+
+`
