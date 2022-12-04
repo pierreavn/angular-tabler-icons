87285c34de65094a5543db31b11be0ca19d539ab
pierreavn <pierreavn@users.noreply.github.com>
2022-12-04 17:04:57 +0000

    Tabler Icons v1.115.0

---
+++ projects/angular-tabler-icons/icons/svg/arrow-badge-right.ts
@@ -0,0 +1,7 @@
+export const IconArrowBadgeRight = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M13 7h-6l4 5l-4 5h6l4 -5z" />
+</svg>
+
+
+`
--- projects/angular-tabler-icons/icons/svg/brand-national-geographic.ts
+++ projects/angular-tabler-icons/icons/svg/brand-national-geographic.ts
@@ -1,6 +1,6 @@
 export const IconBrandNationalGeographic = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M6.375 3h11.25v18h-11.25z" />
+  <path d="M7 3h10v18h-10z" />
 </svg>
 
 
