d9bf68a652f1be7d43aa026157bc5afa5f2c156a
pierreavn <pierreavn@users.noreply.github.com>
2022-12-08 07:56:36 +0000

    Tabler Icons v1.116.1

---
+++ projects/angular-tabler-icons/icons/svg/square-chevron-up.ts
@@ -0,0 +1,8 @@
+export const IconSquareChevronUp = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M20 6.667v10.666a2.667 2.667 0 0 1 -2.667 2.667h-10.666a2.667 2.667 0 0 1 -2.667 -2.667v-10.666a2.667 2.667 0 0 1 2.667 -2.667h10.666a2.667 2.667 0 0 1 2.667 2.667z" />
+  <path d="M9 13l3 -3l3 3" />
+</svg>
+
+
+`
