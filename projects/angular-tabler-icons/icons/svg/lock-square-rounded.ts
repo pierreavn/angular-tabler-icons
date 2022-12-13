2b4a44fcea52a2d140c91aea55521a7fcf53edb3
pierreavn <pierreavn@users.noreply.github.com>
2022-12-13 19:42:03 +0000

    Tabler Icons v1.117.0

---
+++ projects/angular-tabler-icons/icons/svg/lock-square-rounded.ts
@@ -0,0 +1,9 @@
+export const IconLockSquareRounded = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
+  <rect x="8" y="11" width="8" height="5" rx="1" />
+  <path d="M10 11v-2a2 2 0 1 1 4 0v2" />
+</svg>
+
+
+`
