4a686e10020638e6133dd716c0112dbfa352a07f
pierreavn <pierreavn@users.noreply.github.com>
2023-02-04 12:37:19 +0000

    Tabler Icons v2.2.0

--- projects/angular-tabler-icons/icons/svg/braille.ts
+++ projects/angular-tabler-icons/icons/svg/braille.ts
@@ -1,4 +1,4 @@
-export const IconBraile = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+export const IconBraille = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M15 5a1 1 0 1 0 2 0a1 1 0 0 0 -2 0z" />
   <path d="M7 5a1 1 0 1 0 2 0a1 1 0 0 0 -2 0z" />
--- projects/angular-tabler-icons/icons/svg/headset.ts
+++ projects/angular-tabler-icons/icons/svg/headset.ts
@@ -1,9 +1,9 @@
 export const IconHeadset = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M4 13m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
-  <path d="M16 13m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
-  <path d="M4 15v-3a8 8 0 0 1 16 0v3" />
-  <path d="M18 19a6 3 0 0 1 -6 3" />
+  <path d="M4 14v-3a8 8 0 1 1 16 0v3" />
+  <path d="M18 19c0 1.657 -2.686 3 -6 3" />
+  <path d="M4 14a2 2 0 0 1 2 -2h1a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-1a2 2 0 0 1 -2 -2v-3z" />
+  <path d="M15 14a2 2 0 0 1 2 -2h1a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-1a2 2 0 0 1 -2 -2v-3z" />
 </svg>
 
 
