3ce4f4459a61b8572d3ce05e9f3d2fd1253b292c
pierreavn <pierreavn@users.noreply.github.com>
2023-02-25 13:25:38 +0000

    Tabler Icons v2.5.0

---
+++ projects/angular-tabler-icons/icons/svg/brand-framer-motion.ts
@@ -0,0 +1,8 @@
+export const IconBrandFramerMotion = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12 12l-8 -8v16l16 -16v16l-4 -4" />
+  <path d="M20 12l-8 8l-4 -4" />
+</svg>
+
+
+`
--- projects/angular-tabler-icons/icons/svg/square-number-5.ts
+++ projects/angular-tabler-icons/icons/svg/square-number-5.ts
@@ -1,6 +1,6 @@
 export const IconSquareNumber5 = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+  <path d="M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
   <path d="M10 15a1 1 0 0 0 1 1h2a1 1 0 0 0 1 -1v-2a1 1 0 0 0 -1 -1h-3v-4h4" />
 </svg>
 
