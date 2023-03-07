ac8a01f1dd2e7c5c0bc8c54e4566c9290290f147
pierreavn <pierreavn@users.noreply.github.com>
2023-03-07 07:43:48 +0000

    Tabler Icons v2.8.0

---
+++ projects/angular-tabler-icons/icons/svg/clock-down.ts
@@ -0,0 +1,10 @@
+export const IconClockDown = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M21 12a9 9 0 1 0 -9 9" />
+  <path d="M12 7v5l1 1" />
+  <path d="M19 16v6" />
+  <path d="M22 19l-3 3l-3 -3" />
+</svg>
+
+
+`
