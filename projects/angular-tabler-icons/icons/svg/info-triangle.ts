c944e2e8d4f01a8c3673d87ec415ee4d736d130e
pierreavn <pierreavn@users.noreply.github.com>
2023-03-12 01:27:16 +0000

    Tabler Icons v2.10.0

---
+++ projects/angular-tabler-icons/icons/svg/info-triangle.ts
@@ -0,0 +1,9 @@
+export const IconInfoTriangle = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12 10h.01" />
+  <path d="M11 13h1v4h1" />
+  <path d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+</svg>
+
+
+`
