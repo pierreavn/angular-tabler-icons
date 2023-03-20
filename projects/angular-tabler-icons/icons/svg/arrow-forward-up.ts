0741300998659bc400bc211ce3f79e6d7584aba8
pierreavn <pierreavn@users.noreply.github.com>
2023-03-20 12:51:10 +0000

    Tabler Icons v2.11.0

--- projects/angular-tabler-icons/icons/svg/arrow-forward-up.ts
+++ projects/angular-tabler-icons/icons/svg/arrow-forward-up.ts
@@ -1,6 +1,7 @@
 export const IconArrowForwardUp = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M15 13l4 -4l-4 -4m4 4h-11a4 4 0 0 0 0 8h1" />
+  <path d="M15 14l4 -4l-4 -4" />
+  <path d="M19 10h-11a4 4 0 1 0 0 8h1" />
 </svg>
 
 
