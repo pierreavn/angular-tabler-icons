941b54dee7d4afdcd832ee6dbf8ea6987d4059ae
pierreavn <pierreavn@users.noreply.github.com>
2023-05-11 20:31:08 +0000

    Tabler Icons v2.18.0

---
+++ projects/angular-tabler-icons/icons/svg/wash-dry-flat.ts
@@ -0,0 +1,8 @@
+export const IconWashDryFlat = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M3 6a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v12a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3v-12z" />
+  <path d="M7 12h10" />
+</svg>
+
+
+`
