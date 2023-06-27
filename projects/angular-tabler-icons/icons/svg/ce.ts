5a35ad29b1b0e00cf125c3a5e6ee94d94e78bceb
pierreavn <pierreavn@users.noreply.github.com>
2023-06-27 19:09:15 +0000

    Tabler Icons v2.23.0

--- projects/angular-tabler-icons/icons/svg/ce.ts
+++ projects/angular-tabler-icons/icons/svg/ce.ts
@@ -1,8 +1,8 @@
 export const IconCe = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M11 4a8 8 0 1 0 0 16" />
-  <path d="M20 4a8 8 0 1 0 0 16" />
-  <path d="M12 12l8 0" />
+  <path d="M9 6a6 6 0 1 0 0 12" />
+  <path d="M21 6a6 6 0 1 0 0 12" />
+  <path d="M15 12h6" />
 </svg>
 
 
