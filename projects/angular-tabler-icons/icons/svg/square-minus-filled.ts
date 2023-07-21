6ec17b1c64286ad994e4f6cefc421874480c5980
pierreavn <pierreavn@users.noreply.github.com>
2023-07-21 06:58:36 +0000

    Tabler Icons v2.27.0

---
+++ projects/angular-tabler-icons/icons/svg/square-minus-filled.ts
@@ -0,0 +1,7 @@
+export const IconSquareMinusFilled = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M19 2a3 3 0 0 1 3 3v14a3 3 0 0 1 -3 3h-14a3 3 0 0 1 -3 -3v-14a3 3 0 0 1 3 -3zm-4 9h-6l-.117 .007a1 1 0 0 0 .117 1.993h6l.117 -.007a1 1 0 0 0 -.117 -1.993z" stroke-width="0" fill="currentColor" />
+</svg>
+
+
+`
