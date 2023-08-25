63584f9bda6162734cf798d7c5c7647e8c7fc0d6
pierreavn <pierreavn@users.noreply.github.com>
2023-08-25 17:31:45 +0000

    Tabler Icons v2.32.0

---
+++ projects/angular-tabler-icons/icons/svg/map-north.ts
@@ -0,0 +1,8 @@
+export const IconMapNorth = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+  <path d="M10 15v-6l4 6v-6" />
+</svg>
+
+
+`
