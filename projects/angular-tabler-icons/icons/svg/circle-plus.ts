8954c80fb9cc52a69f26bcee5d3f07b44ebc4b61
pierreavn <pierreavn@users.noreply.github.com>
2023-09-06 10:27:26 +0000

    Tabler Icons v2.33.0

--- projects/angular-tabler-icons/icons/svg/circle-plus.ts
+++ projects/angular-tabler-icons/icons/svg/circle-plus.ts
@@ -1,8 +1,8 @@
 export const IconCirclePlus = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M20.987 12.497a9 9 0 1 0 -8.467 8.489" />
-  <path d="M16 19h6" />
-  <path d="M19 16v6" />
+  <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+  <path d="M9 12h6" />
+  <path d="M12 9v6" />
 </svg>
 
 
