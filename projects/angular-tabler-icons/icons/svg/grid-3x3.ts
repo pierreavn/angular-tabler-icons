c292226ce4788e785c242e0078acfac06ccec81b
pierreavn <pierreavn@users.noreply.github.com>
2023-10-03 06:19:23 +0000

    Tabler Icons v2.36.0

---
+++ projects/angular-tabler-icons/icons/svg/grid-3x3.ts
@@ -0,0 +1,10 @@
+export const IconGrid3x3 = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M3 8h18" />
+  <path d="M3 16h18" />
+  <path d="M8 3v18" />
+  <path d="M16 3v18" />
+</svg>
+
+
+`
