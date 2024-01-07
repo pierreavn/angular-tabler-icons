da3c961c36f0eaf33d4785eaff2c09302dbf094f
pierreavn <pierreavn@users.noreply.github.com>
2024-01-07 11:57:06 +0000

    Tabler Icons v2.45.0

---
+++ projects/angular-tabler-icons/icons/svg/brand-pnpm.ts
@@ -0,0 +1,14 @@
+export const IconBrandPnpm = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M3 17h4v4h-4z" />
+  <path d="M10 17h4v4h-4z" />
+  <path d="M17 17h4v4h-4z" />
+  <path d="M17 10h4v4h-4z" />
+  <path d="M17 3h4v4h-4z" />
+  <path d="M10 10h4v4h-4z" />
+  <path d="M10 3h4v4h-4z" />
+  <path d="M3 3h4v4h-4z" />
+</svg>
+
+
+`
---
+++ projects/angular-tabler-icons/icons/svg/square-percentage.ts
@@ -0,0 +1,10 @@
+export const IconSquarePercentage = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M9 15l6 -6" />
+  <path d="M9.5 9.5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+  <path d="M14.5 14.5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+  <path d="M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z" />
+</svg>
+
+
+`
