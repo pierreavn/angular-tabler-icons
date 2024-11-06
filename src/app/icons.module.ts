0337b35cf39e079e6ae2216c618fcb821c3b1c50
The Ult <ult_dev@pm.me>
2024-11-06 15:08:01 +0100

    chore(sample-app): little cleanup

--- src/app/icons.module.ts
+++ src/app/icons.module.ts
@@ -1,23 +1,25 @@
 import { NgModule } from '@angular/core';
 
 import { TablerIconsModule } from 'angular-tabler-icons';
-import { IconCamera, IconCameraFilled, IconHeart, IconHeartFilled, IconBrandGithub } from 'angular-tabler-icons/icons';
-
-// Select some icons (use an object, not an array)
-const icons = {
+import {
   IconCamera,
   IconCameraFilled,
   IconHeart,
   IconHeartFilled,
-  IconBrandGithub
-};
+  IconBrandGithub,
+} from 'angular-tabler-icons/icons';
+
+// Select some icons (use an object, not an array)
+const ICONS = {
+  IconCamera,
+  IconCameraFilled,
+  IconHeart,
+  IconHeartFilled,
+  IconBrandGithub,
+} as const;
 
 @NgModule({
-  imports: [
-    TablerIconsModule.pick(icons)
-  ],
-  exports: [
-    TablerIconsModule
-  ]
+  imports: [TablerIconsModule.pick(ICONS)],
+  exports: [TablerIconsModule],
 })
-export class IconsModule { }
+export class IconsModule {}
