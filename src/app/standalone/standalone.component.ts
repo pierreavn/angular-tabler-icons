9406791430d48df1f3156d18f7582f9bd8cbf7a1
The Ult <ult_dev@pm.me>
2024-11-06 15:31:43 +0100

    chore(sample-app): use sfc or standalone component

--- src/app/standalone/standalone.component.ts
+++ src/app/standalone/standalone.component.ts
@@ -8,15 +8,40 @@ import {
 
 @Component({
   selector: 'app-standalone',
-  templateUrl: './standalone.component.html',
   standalone: true,
   imports: [TablerIconComponent],
   providers: [
+    /**
+     * Provide the icons which you want to use in this component.
+     */
     provideTablerIcons({
       IconNumber123,
       IconAdOff,
       IconHeartFilled,
     }),
   ],
+  template: `
+    <h2>Standalone component sample</h2>
+
+    <fieldset>
+      <legend>123</legend>
+      <i-tabler name="number-123"></i-tabler>
+    </fieldset>
+
+    <fieldset>
+      <legend>123 (style, big)</legend>
+      <i-tabler name="number-123" style="height: 60px; width: 60px;"></i-tabler>
+    </fieldset>
+
+    <fieldset>
+      <legend>ad-off (style, red)</legend>
+      <i-tabler name="ad-off" style="color: red;"></i-tabler>
+    </fieldset>
+
+    <fieldset>
+      <legend>heart-filled (style, red)</legend>
+      <i-tabler name="heart-filled" style="color: red;"></i-tabler>
+    </fieldset>
+  `,
 })
 export class StandaloneComponent {}
