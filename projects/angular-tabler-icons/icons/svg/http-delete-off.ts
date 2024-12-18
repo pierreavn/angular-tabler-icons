6c6ebaa4bd82c80e2dd586b59bf6501d9981b0d3
pierreavn <pierreavn@users.noreply.github.com>
2024-12-18 19:30:42 +0000

    Tabler Icons v3.26.0

---
+++ projects/angular-tabler-icons/icons/svg/http-delete-off.ts
@@ -0,0 +1,1 @@
+export const IconHttpDeleteOff = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" >  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>  <path d="M3 8v8h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2z" />  <path d="M14 8h-2m-2 2v6h4" />  <path d="M10 12h2" />  <path d="M17 8v5m3 3h1" />  <path d="M3 3l18 18" /></svg>`
