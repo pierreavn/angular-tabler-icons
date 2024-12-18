14547b97f4806cc190d227b04751954f81aba088
pierreavn <pierreavn@users.noreply.github.com>
2024-12-18 19:24:09 +0000

    Tabler Icons v3.25.0

--- projects/angular-tabler-icons/icons/svg/seeding-filled.ts
+++
@@ -1,1 +0,0 @@
-export const IconSeedingFilled = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor">  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>  <path d="M6 3a7 7 0 0 1 6.95 6.155a6.97 6.97 0 0 1 5.05 -2.155h3a1 1 0 0 1 1 1v1a7 7 0 0 1 -7 7h-2v4a1 1 0 0 1 -2 0v-7h-2a7 7 0 0 1 -7 -7v-2a1 1 0 0 1 1 -1z" /></svg>`
