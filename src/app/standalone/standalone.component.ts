af9abdc4561058c6e4bf2077f94731d51988a60a
The Ult <ult_dev@pm.me>
2024-12-23 10:29:54 +0100

    refactor(sample-app): update component templates to replace <i-tabler> with <tabler-icon> syntax

--- src/app/standalone/standalone.component.ts
+++ src/app/standalone/standalone.component.ts
@@ -8,46 +8,49 @@ import {
 } from 'angular-tabler-icons/icons';
 
 @Component({
-    selector: 'app-standalone',
-    imports: [TablerIconComponent],
-    providers: [
-        /**
-         * Provide the icons which you want to use in this component.
-         */
-        provideTablerIcons({
-            IconNumber123,
-            IconAdOff,
-            IconHeartFilled,
-            IconGolfFilled,
-        }),
-    ],
-    template: `
+  selector: 'app-standalone',
+  imports: [TablerIconComponent],
+  providers: [
+    /**
+     * Provide the icons which you want to use in this component.
+     */
+    provideTablerIcons({
+      IconNumber123,
+      IconAdOff,
+      IconHeartFilled,
+      IconGolfFilled,
+    }),
+  ],
+  template: `
     <h2>Standalone component sample</h2>
 
     <fieldset>
       <legend>123</legend>
-      <i-tabler name="number-123"></i-tabler>
+      <tabler-icon name="number-123"></tabler-icon>
     </fieldset>
 
     <fieldset>
       <legend>123 (style, big)</legend>
-      <i-tabler name="number-123" style="height: 60px; width: 60px;"></i-tabler>
+      <tabler-icon
+        name="number-123"
+        style="height: 60px; width: 60px;"
+      ></tabler-icon>
     </fieldset>
 
     <fieldset>
       <legend>ad-off (style, red)</legend>
-      <i-tabler name="ad-off" style="color: red;"></i-tabler>
+      <tabler-icon name="ad-off" style="color: red;"></tabler-icon>
     </fieldset>
 
     <fieldset>
       <legend>heart-filled (style, red)</legend>
-      <i-tabler name="heart-filled" style="color: red;"></i-tabler>
+      <tabler-icon name="heart-filled" style="color: red;"></tabler-icon>
     </fieldset>
 
     <fieldset>
       <legend>golf-filled (style, blue)</legend>
-      <i-tabler name="golf-filled" style="color: blue;"></i-tabler>
+      <tabler-icon name="golf-filled" style="color: blue;"></tabler-icon>
     </fieldset>
-  `
+  `,
 })
 export class StandaloneComponent {}
